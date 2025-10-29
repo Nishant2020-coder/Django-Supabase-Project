af93fb3cf6f2bf616ddea6e00e0fa67a509eec48
Nishant Shandilya <nishantshandilya@Nishants-MacBook-Air.local>
2025-10-30 00:08:58 +0530

    Use remote Postgres database

--- config/settings.py
+++ config/settings.py
@@ -83,15 +83,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
+
 
 
 # Password validation
